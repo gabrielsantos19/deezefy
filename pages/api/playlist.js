a27d2d22e5a0194d61c6ea430e58c94b0c8df80a
gabrielsantos19 <25865943+gabrielsantos19@users.noreply.github.com>
2021-01-18 12:48:52 -0300

    Criar playlist CRUD

---
+++ pages/api/playlist.js
@@ -0,0 +1,99 @@
+import { pool } from "../../lib/pool.js"
+
+
+export default (req, res) => {
+  if(req.method == "GET") {
+    return get(req, res)
+  }
+  else if (req.method == "POST") {
+    return post(req, res)
+  }
+  else if (req.method == "PUT") {
+    return put(req, res)
+  }
+  else if (req.method == "DELETE") {
+    return deleteMethod(req, res)
+  }
+  else {
+    return res.status(405).end()
+  }
+}
+
+async function get(req, res) {
+  await pool.query(`SELECT * FROM playlist`)
+  .then(results => {
+    console.log(results)
+    res.status(200).json({
+      playlists: results.rows,
+    })
+  })
+  .catch(error => {
+    console.error(error)
+    res.status(500).end()
+  })
+}
+
+async function post(req, res) {
+  const playlist = req.body
+
+  await pool.query(`INSERT INTO playlist
+                    (nome, status, criador, data_de_criacao)
+                    VALUES ($1, $2, $3, $4)`, 
+                    [
+                        playlist.nome, 
+                        playlist.status, 
+                        playlist.criador, 
+                        playlist.data_de_criacao
+                    ])
+  .then(results => {
+    console.log(results)
+    res.status(201).end()
+  })
+  .catch(error => {
+    console.error(error)
+    res.status(500).end()
+  })
+}
+
+async function put(req, res) {
+  const playlist = req.body
+
+  await pool.query(`UPDATE playlist SET
+                    nome = $1, 
+                    status = $2, 
+                    criador = $3, 
+                    data_de_criacao = $4
+                    WHERE nome = $1`,
+                    [
+                      playlist.nome, 
+                      playlist.status, 
+                      playlist.criador, 
+                      playlist.data_de_criacao
+                    ])
+  .then(results => {
+    console.log(results)
+    res.status(200).end()
+  })
+  .catch(error => {
+    console.error(error)
+    res.status(500).end()
+  })
+}
+
+async function deleteMethod(req, res) {
+  const playlist = req.body
+  
+  await pool.query(`DELETE FROM playlist 
+                    WHERE nome = $1`, 
+                    [
+                        playlist.nome
+                    ])
+  .then(results => {
+    console.log(results)
+    res.status(200).end()
+  })
+  .catch(error => {
+    console.error(error)
+    res.status(500).end()
+  })
+}
