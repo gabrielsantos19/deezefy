ca976b4498ea4c481e498011106c116cb5d981e0
gabrielsantos19 <25865943+gabrielsantos19@users.noreply.github.com>
2021-01-17 23:05:22 -0300

    Criar ouvinte CRUD

---
+++ pages/api/ouvinte.js
@@ -0,0 +1,95 @@
+import { pool } from "../../lib/pool.js"
+import * as usuario from "../../lib/usuario.js"
+
+
+export default (req, res) => {
+  if(req.method == "GET") {
+    return get(req, res)
+  }
+  else if (req.method == "POST") {
+    return post(req, res)
+  }
+  else if (req.method == "PUT") {
+    return put(req, res)
+  }
+  else if (req.method == "DELETE") {
+    return deleteMethod(req, res)
+  }
+  else {
+    return res.status(405).end()
+  }
+}
+
+async function get(req, res) {
+  pool.query(`SELECT * FROM ouvinte
+              JOIN usuario ON usuario.email = ouvinte.usuario`)
+  .then(results => {
+    console.log(results)
+    res.status(200).json({
+      ouvintes: results.rows,
+    })
+  })
+  .catch(error => {
+    console.error(error)
+    res.status(500).end()
+  })
+}
+
+async function post(req, res) {
+  const ouvinte = req.body
+  
+  await usuario.post(ouvinte)
+  .then(results => {})
+  .catch(error => {})
+
+  await pool.query(`INSERT INTO ouvinte
+              (primeiro_nome, sobrenome, telefone, usuario)
+              VALUES ($1, $2, $3, $4)`, 
+              [
+                ouvinte.primeiro_nome,
+                ouvinte.sobrenome,
+                ouvinte.telefone,
+                ouvinte.email
+              ])
+  .then(results => {
+    console.log(results)
+    res.status(201).end()
+  })
+  .catch(error => {
+    console.error(error)
+    res.status(500).end()
+  })
+}
+
+async function put(req, res) {
+  const ouvinte = req.body
+
+  await usuario.put(ouvinte)
+  .then(results => {
+    console.log(results)
+    res.status(200).end()
+  })
+  .catch(error => {
+    console.error(error)
+    res.status(500).end()
+  })
+}
+
+async function deleteMethod(req, res) {
+  const ouvinte = req.body
+  
+  await pool.query(`DELETE FROM ouvinte 
+              WHERE usuario = $1`, 
+              [
+                ouvinte.email
+              ]
+  )
+  .then(results => {
+    console.log(results)
+    res.status(200).end()
+  })
+  .catch(error => {
+    console.error(error)
+    res.status(500).end()
+  })
+}
