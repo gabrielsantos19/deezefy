bb56a92a49e18a2c64c76b0e7defb94c31d46170
gabrielsantos19 <25865943+gabrielsantos19@users.noreply.github.com>
2021-01-26 16:01:25 -0300

    Criar api para o relacionamento Segue

---
+++ pages/api/segue.js
@@ -0,0 +1,81 @@
+import { pool } from "../../lib/pool.js"
+
+
+export default (req, res) => {
+  if(req.method == "GET") {
+    return get(req, res)
+  }
+  else if (req.method == "POST") {
+    return post(req, res)
+  }
+  else if (req.method == "PUT") {
+    return put(req, res)
+  }
+  else if (req.method == "DELETE") {
+    return deleteMethod(req, res)
+  }
+  else {
+    return res.status(405).end()
+  }
+}
+
+async function get(req, res) {
+  await pool.query(`SELECT * FROM segue`)
+  .then(results => {
+    console.log(results)
+    res.status(200).json({
+      seguidos: results.rows
+    })
+  })
+  .catch(error => {
+    console.error(error)
+    res.status(500).end()
+  })
+}
+
+async function post(req, res) {
+  const segue = req.body
+
+  await pool.query(
+    `INSERT INTO segue
+    (artista, ouvinte)
+    VALUES ($1, $2)`, 
+    [
+      segue.artista,
+      segue.ouvinte
+    ]
+  )
+  .then(results => {
+    console.log(results)
+    res.status(201).end()
+  })
+  .catch(error => {
+    console.error(error)
+    res.status(500).end()
+  })
+}
+
+async function put(req, res) {
+  res.status(500).end()
+}
+
+async function deleteMethod(req, res) {
+  const segue = req.body
+
+  await pool.query(
+    `DELETE FROM segue 
+    WHERE artista = $1 AND ouvinte = $2`, 
+    [
+      segue.artista,
+      segue.ouvinte
+    ]
+  )
+  .then(results => {
+    console.log(results)
+    res.status(200).end()
+  })
+  .catch(error => {
+    console.error(error)
+    res.status(500).end()
+  })
+}
