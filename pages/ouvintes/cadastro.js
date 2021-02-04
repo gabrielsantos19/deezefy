57c65334169687ec35c1590274cece2d80e9e295
gabrielsantos19 <25865943+gabrielsantos19@users.noreply.github.com>
2021-02-04 17:38:14 -0300

    Centralizar formulario

--- pages/ouvintes/cadastro.js
+++ pages/ouvintes/cadastro.js
@@ -3,13 +3,15 @@ import SideBar from '../../components/sidebar';
 import style from '../../styles/Cadastro.module.css'
 
 
-export default function Login() {
+export default function Cadastro() {
   const [email, setEmail] = useState('')
   const [senha, setSenha] = useState('')
   const [primeiroNome, setPrimeiroNome] = useState('')
   const [sobrenome, setSobrenome] = useState('')
   const [telefone, setTelefone] = useState('')
+
   const [disponivel, setDisponivel] = useState(true)
+
 
   async function cadastrar() {
     setDisponivel(false);
@@ -32,47 +34,67 @@ export default function Login() {
     })
   }
 
+  async function submitHandler(e) {
+    e.preventDefault()
+    cadastrar()
+  }
+
   return (
     <main>
-      <div>
-        <h1>Cadastro</h1>
+      <div className={ style.container }>
+        <h1>Cadastro de Ouvinte</h1>
   
-        <label>E-mail</label>
-        <input type='text' 
-          value={ email }        
-          onChange={ e => setEmail(e.target.value) }>
-        </input>
-        
-        <label>Senha</label>
-        <input type='text' 
-          value={ senha }
-          onChange={ e => setSenha(e.target.value) }>
-        </input>
-
-        <label>Primeiro nome</label>
-        <input type='text' 
-          value={ primeiroNome }
-          onChange={ e => setPrimeiroNome(e.target.value) }>
-        </input>
-        
-        <label>Sobrenome</label>
-        <input type='text' 
-          value={ sobrenome }
-          onChange={ e => setSobrenome(e.target.value) }>
-        </input>
-        
-        <label>Telefone</label>
-        <input type='text' 
-          value={ telefone }
-          onChange={ e => setTelefone(e.target.value) }>
-        </input>
-
-        <button onClick={ cadastrar } 
-          disabled={ !disponivel }
-          type='submit'>
-          Cadastrar
-        </button>
+        <form className={ style.formulario } onSubmit={ submitHandler }>
+          <div className={ style.campo }>
+            <label>E-mail</label>
+            <input type='text' 
+              value={ email }        
+              onChange={ e => setEmail(e.target.value) }>
+            </input>
+          </div>
+          
+          <div className={ style.campo }>
+            <label>Senha</label>
+            <input type='text' 
+              value={ senha }
+              onChange={ e => setSenha(e.target.value) }>
+            </input>
+          </div>
+    
+          <div className={ style.campo }>
+            <label>Primeiro nome</label>
+            <input type='text' 
+              value={ primeiroNome }
+              onChange={ e => setPrimeiroNome(e.target.value) }>
+            </input>
+          </div>
+          
+          <div className={ style.campo }>
+            <label>Sobrenome</label>
+            <input type='text' 
+              value={ sobrenome }
+              onChange={ e => setSobrenome(e.target.value) }>
+            </input>
+          </div>
+          
+          <div className={ style.campo }>
+            <label>Telefone</label>
+            <input type='text' 
+              value={ telefone }
+              onChange={ e => setTelefone(e.target.value) }>
+            </input>
+          </div>
+    
+          <div className={ style.botaoEnviar }>
+            <button 
+              disabled={ !disponivel }
+              type='submit'>
+              Cadastrar
+            </button>
+          </div>
+        </form>
       </div>
+      
 
       <SideBar></SideBar>
     </main>
