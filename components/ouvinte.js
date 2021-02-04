d8d8a211f9f9e4cf190f70cc3921e180aabc6815
gabrielsantos19 <25865943+gabrielsantos19@users.noreply.github.com>
2021-02-04 12:02:59 -0300

    Adicionar funcionalidade de remover ouvinte

--- components/ouvinte.js
+++ components/ouvinte.js
@@ -4,8 +4,26 @@ import style from '../styles/Ouvinte.module.css'
 
 
 export default function Ouvinte({ ouvinte }) {
+  const [deletado, setDeletado] = useState(false)
+
+  function remover() {
+    if(deletado) return
+
+    fetch('/api/ouvinte', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify ({
+        email: ouvinte.email
+      })
+    })
+    .then(response => setDeletado(true))
+    .catch(error => {})
+  }
+
   return (
-    <div className={style.ouvinte}>
+    <div className={ `${style.ouvinte} ${deletado ? style.ouvinteDeletado : ''}` }>
       <div className={ style.nome }>
         { ouvinte.primeiro_nome } { ouvinte.sobrenome }
       </div>
@@ -14,6 +32,11 @@ export default function Ouvinte({ ouvinte }) {
       </div>
       <div className={ style.telefone }>
         { ouvinte.telefone }
+      </div>
+      <div className={ style.menu }>
+        <button onClick={ remover }>
+          Remover
+        </button>
       </div>
       
     </div>
