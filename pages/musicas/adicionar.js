5a94ab86de285785d476edfb2181033ef839af2c
gabrielsantos19 <25865943+gabrielsantos19@users.noreply.github.com>
2021-02-07 15:38:08 -0300

    Corrigir fetch para /api/artistas

--- pages/musicas/adicionar.js
+++ pages/musicas/adicionar.js
@@ -11,7 +11,7 @@ export default function AdicionarMusica() {
   const [disponivel, setDisponivel] = useState(true);
 
   useEffect(() => {
-    fetch('/api/artista')
+    fetch('/api/artistas')
     .then(response => response.json())
     .then(json => setArtistas(json.artistas))
     .catch(error => {})
