a0d94de0717eebe5b37cfada42892177d76822da
gabrielsantos19 <25865943+gabrielsantos19@users.noreply.github.com>
2021-02-08 00:36:21 -0300

    Converter campo artistas para multivalorado

--- pages/musicas/adicionar.js
+++ pages/musicas/adicionar.js
@@ -4,8 +4,8 @@ import styles from '../../styles/AdicionarMusica.module.css'
 
 
 export default function AdicionarMusica() {
+  const [todosArtistas, setTodosArtistas] = useState([])
   const [artistas, setArtistas] = useState([])
-  const [artista, setArtista] = useState('')
   const [nome, setNome] = useState('');
   const [duracao, setDuracao] = useState('');
   const [disponivel, setDisponivel] = useState(true);
@@ -13,23 +13,21 @@ export default function AdicionarMusica() {
   useEffect(() => {
     fetch('/api/artistas')
     .then(response => response.json())
-    .then(json => setArtistas(json.artistas))
+    .then(json => setTodosArtistas(json.artistas))
     .catch(error => {})
   }, [])
 
   async function adicionar() {
     setDisponivel(false);
-    fetch('/api/grava', {
+    fetch('/api/musica', {
       method: 'POST', 
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        artista: artista,
-        musica: {
-          nome: nome,
-          duracao: duracao
-        }
+        nome: nome,
+        duracao: duracao,
+        artistas: artistas
       })
     })
     .then(response => {
@@ -39,22 +37,56 @@ export default function AdicionarMusica() {
     })
   }
 
-  return (
-    <main>
-      <h1>Adicionar Música</h1>
-
-      <div className={styles.form}>
-        <label>Artista</label>
-        <select onChange={ e => setArtista(e.target.value) }>
+  function artistaInput(index) {
+    return (
+      <div key={ index }>
+        <label>Artista { index+1 }</label>
+        <select 
+            key={ index }
+            value={ artistas[index] }
+            onChange={ e => setArtistaInput(index, e.target.value) }>
+          <option selected disabled hidden></option>
           {
-            artistas.map(a => (
-              <option key={a.usuario} value={a.usuario}>
+            todosArtistas.map(a => (
+              <option key={index + a.usuario} value={a.usuario}>
                 {a.nome_artistico}
               </option>
             ))
           }
         </select>
+        <button onClick={ () => removerArtistaInput(index) }>
+          Remover { index }
+        </button>
+      </div>
+    )
+  }
 
+  function setArtistaInput(index, valor) {
+    let novosArtistas = [...artistas]
+
+    if(index == artistas.length) {
+      novosArtistas.push(valor)
+    }
+    else
+    {
+      novosArtistas[index] = valor
+    }
+    setArtistas(novosArtistas)
+  }
+
+  function removerArtistaInput(index) {
+    if(artistas.length > 1) {
+      let novosArtistas = [...artistas]
+      novosArtistas.splice(index, 1)
+      setArtistas(novosArtistas)
+    }
+  }
+
+  return (
+    <main>
+      <h1>Adicionar Música</h1>
+
+      <div className={styles.form}>
         <label>Nome da música</label>
         <input type='text' 
           value={ nome }        
@@ -67,6 +99,13 @@ export default function AdicionarMusica() {
           onChange={ e => setDuracao(e.target.value) }>
         </input>
         
+        {
+          artistas.map((a, i) => artistaInput(i))
+        }
+        {
+          artistaInput(artistas.length)
+        }
+
         <button onClick={ adicionar } 
           disabled={ !disponivel }
           type='submit'>Enviar
